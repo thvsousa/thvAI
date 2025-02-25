b7ab18bf177ce97d6a32c153b6ea904e9c9280b7
Thiago Sousa <thvsousa@gmail.com>
2025-02-25 06:27:44 -0300

    Movendo arquivos e corrigindo estrutura

--- ai-chat/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css'; // Importando o CSS global
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
